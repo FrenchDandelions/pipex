dc85ce71f57f59712e44c4b9d8ee7b110358ea89
LE Thomas <thomas.le.tle@gmail.com>
2024-02-22 16:41:31 +0100

    modified

--- src/fork.c
+++ src/fork.c
@@ -12,15 +12,19 @@
 
 #include "pipex.h"
 
-void	fork_child1(char *cmd, char **env, int *fds)
+void	fork_child1(char *cmd, char **env, int *fds, char *s)
 {
 	pid_t	pid;
+	int		fd_in;
 
 	pid = fork();
 	if (pid == -1)
 		exit_error("fork\n");
 	else if (pid == 0)
 	{
+		fd_in = open_file(s, 0);
+		dup2(fd_in, 0);
+		close(fd_in);
 		close(fds[0]);
 		dup2(fds[1], STDOUT_FILENO);
 		close(fds[1]);
@@ -35,15 +39,19 @@ void	fork_child1(char *cmd, char **env, int *fds)
 	}
 }
 
-void	fork_child2(char *cmd, char **env, int *fds)
+void	fork_child2(char *cmd, char **env, int *fds, char *s)
 {
 	pid_t	pid;
+	int		fd_out;
 
 	pid = fork();
 	if (pid == -1)
 		exit_error("fork\n");
 	else if (pid == 0)
 	{
+		fd_out = open_file(s, 1);
+		dup2(fd_out, 1);
+		close(fd_out);
 		close(fds[0]);
 		close(fds[1]);
 		exec(cmd, env);
--- src/main.c
+++ src/main.c
@@ -26,8 +26,6 @@ int	wait_pid(int *fds)
 
 int	main(int argc, char **argv, char **env)
 {
-	int	fd_in;
-	int	fd_out;
 	int	fds[2];
 
 	if (argc != 5)
@@ -35,17 +33,11 @@ int	main(int argc, char **argv, char **env)
 			1);
 	if (!env[0])
 		return (0);
-	fd_in = open_file(argv[1], 0);
-	dup2(fd_in, 0);
-	close(fd_in);
 	if (pipe(fds) == -1)
 		exit_error("pipe\n");
-	fork_child1(argv[2], env, fds);
+	fork_child1(argv[2], env, fds, argv[1]);
 	if (pipe(fds) == -1)
 		exit_error("pipe\n");
-	fd_out = open_file(argv[argc - 1], 1);
-	dup2(fd_out, 1);
-	close(fd_out);
-	fork_child2(argv[argc - 2], env, fds);
+	fork_child2(argv[argc - 2], env, fds, argv[argc - 1]);
 	return (wait_pid(fds));
 }
